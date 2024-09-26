97a95d33f09dbfc91c5a5a691f9cd5eb08d89aea
jsemalvarez <jsemalvarez@gmail.com>
2024-09-26 14:33:39 -0300

    feat: firebase config

---
+++ src/app/firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  databaseURL: import.meta.env.DATABASE_URL,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+};
+
+
+// Initialize Firebase
+export const FirebaseApp  = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth( FirebaseApp );
+export const FirebaseDB   = getFirestore( FirebaseApp );
