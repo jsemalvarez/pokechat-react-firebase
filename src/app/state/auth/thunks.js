fd264d3168c6b1058d1a210ee224bb535c25b3c0
jsemalvarez <jsemalvarez@gmail.com>
2024-09-27 17:21:36 -0300

    feat: auth thunks

---
+++ src/app/state/auth/thunks.js
@@ -0,0 +1,36 @@
+import { singInWithGoogle } from "../../firebase/authProvider"
+import { saveUser } from "../../firebase/firestoreProvider"
+import { checkingCredentials, login, logout } from "./authSlice"
+
+
+export const checkingAuthentication = ( ) => {
+    return (dispatch) => {
+
+        dispatch( checkingCredentials() )
+
+    }
+}
+
+export const startGoogleSignIn = (  ) => {
+    return async(dispatch) => {
+
+        dispatch( checkingCredentials() )
+        
+        const resultSingIn = await singInWithGoogle()
+        if( !resultSingIn.ok ) return dispatch( logout( resultSingIn.errorMessage ))
+
+        dispatch( login(resultSingIn))
+    }
+}
+
+export const startSaveUser = ( user ) => {
+    return async ( dispatch ) => {
+
+        dispatch( checkingCredentials() )
+
+        const result = await saveUser( user )
+        if( !result.ok ) return dispatch( logout( result.errorMessage ) )
+
+        dispatch( login( result ) )
+    }
+}
