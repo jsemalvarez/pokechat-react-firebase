fa2abc1147e5ccca774c3e9f1e428e32944150ca
jsemalvarez <jsemalvarez@gmail.com>
2024-10-02 16:46:16 -0300

    feat: FirendSearch.jsx

--- src/app/firebase/firestoreProvider.js
+++ src/app/firebase/firestoreProvider.js
@@ -1,4 +1,4 @@
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import { addDoc, arrayUnion, collection, doc, getDoc, getDocs, limit, query, setDoc, updateDoc, where } from "firebase/firestore";
 import { FirebaseDB } from "./config";
 
 
@@ -53,6 +53,94 @@ export const saveUser = async ( newUser) => {
         
         const errorMessage = error.message;
 
+        return {
+            ok: false,
+            errorMessage
+        }
+    }
+
+}
+
+export const getFriendByUserName = async( userName ) => {
+
+    try {
+
+       const customQuery = query(
+           collection( FirebaseDB, "users"),
+           where("userName", "==", userName),
+           limit(1)
+       )
+
+       const querySnapshot = await getDocs(customQuery);
+
+       if (querySnapshot.empty) {
+           throw new Error(`No se encontro ningun usuario con el userName ${userName}`)
+       }
+
+       const friendDB = querySnapshot.docs[0].data();
+
+       return {
+           ok: true,
+           data: friendDB
+       }
+       
+    } catch (error) {
+       const errorMessage = error.message
+       return {
+           ok: false,
+           errorMessage
+       }
+    }
+}
+
+export const addFriend = async (currentUser, newFriend) => {
+
+    try {
+
+        // crear la room
+        const newRoomDB = {
+            messages: [],
+            users: [currentUser?.uid, newFriend.uid],
+        };
+        const roomRef = await addDoc(collection(FirebaseDB, "rooms"), newRoomDB);
+
+        // agregar la room al currentUser
+        const currentUserRoom = {
+            roomid: roomRef.id,
+            lastMessage: "",
+            timestamp: "",
+            friendId: newFriend.uid,
+          };
+
+        const currentUserRef = doc(FirebaseDB, "users", currentUser.uid);
+
+        await updateDoc(currentUserRef, {
+            rooms: arrayUnion(currentUserRoom),
+            friends: arrayUnion(newFriend.uid),
+        });
+    
+        // agregar la room al newUser
+          const newFriendRoom = {
+            roomid: roomRef.id,
+            lastMessage: "",
+            timestamp: "",
+            friendId: currentUser.uid,
+          };
+
+        
+        const newFriendRef = doc(FirebaseDB, "users", newFriend.uid);    
+
+        await updateDoc(newFriendRef, {
+            rooms: arrayUnion(newFriendRoom),
+            friends: arrayUnion(currentUser.uid),
+        });
+        
+        return{
+            ok: true
+        }
+
+    } catch (error) {
+        const errorMessage = error.message
         return {
             ok: false,
             errorMessage
---
+++ src/features/chat/components/FriendSearch.jsx
@@ -0,0 +1,59 @@
+import { useSelector } from "react-redux"
+import { useForm } from "../../../common/hooks/useForm"
+import { addFriend, getFriendByUserName } from "../../../app/firebase/firestoreProvider"
+
+export const FriendSearch = () => {
+
+    const currentUser = useSelector( state => state.auth)
+
+    const { userName, onInputChange } = useForm({
+        userName:''
+    })
+
+    const onSerachFriend = async() => {
+
+        if ( userName.trim() === '') {
+            console.log('userName no puede estar vacio')
+            return;
+        }
+
+        const isMyUserName = currentUser.userName === userName
+        if ( isMyUserName) {
+            console.log('no te puedes agregar a ti mismo')
+            return;
+        }
+
+        const friends = currentUser.friends || []
+        const isAlreadyFriend = friends.find( friend => friend.email === userName)
+        if(isAlreadyFriend){
+            console.log('ya son amigos')
+            return;
+        }
+        
+        const result = await getFriendByUserName(userName)
+
+        if(!result.ok){
+            console.log(result.errorMessage)
+            return;
+        }
+
+        const newFriend = result.data;
+        await addFriend(currentUser, newFriend)
+    }
+
+    return (
+        <div className='friends__search'>
+            <span>*se agregan automaticamente</span>
+            <input 
+                type="text"
+                name="userName"
+                placeholder='nombre del jugador'
+                value={ userName }
+                onChange={ onInputChange }
+            />
+            <button
+                onClick={ onSerachFriend }
+            >Agregar amigo</button>
+        </div>
+  )
+}
