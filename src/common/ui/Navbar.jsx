1ea126ca26d9f780b3226967c4a71974135de0c3
jsemalvarez <jsemalvarez@gmail.com>
2024-10-06 22:32:08 -0300

    feat: Navbar give pargeUrl param

--- src/common/ui/Navbar.jsx
+++ src/common/ui/Navbar.jsx
@@ -1,8 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { startLogOut } from '../../app/state/auth/thunks';
+import { getTeamColorsNavbar } from '../utils/getTeamColor';
 
-export const Navbar = () => {
+export const Navbar = ({pageUrl}) => {
 
     const dispatch = useDispatch();
     const currentUser = useSelector( state => state.auth);
@@ -11,14 +12,18 @@ export const Navbar = () => {
         dispatch( startLogOut() )
     }
 
+    const colorsTeam = getTeamColorsNavbar(currentUser.team);
+
     return (
-        <nav className="navbar">
-        <h2>Pokechat MdP</h2>
-        <div className='navbar__right-items'>
+        <nav className={`navbar ${colorsTeam}`}>
+        <h2 className='navbar__title hide-on-movile'>Pokechat MdP</h2>
+        <div className="navbar__img-content">
             <img src={currentUser.photoURL} alt={currentUser.userName} />
+        </div>
+        <div className='navbar__right-items'>
             <ul>
                 <li><h4 className='user-name'>{currentUser.userName}</h4></li>
-                <li><Link to='/user/config'>config</Link></li>
+                <li><Link to={ pageUrl.path }>{ pageUrl.name }</Link></li>
                 <li>
                     <button 
                         className='btn-close'
